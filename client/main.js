ed3b6aa319f08e37ba88f6c951ed91a5b4835a32
Denis <wolf-den@tut.by>
2021-09-02 14:39:54 +0300

    feat: add username

--- client/main.js
+++ client/main.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+  const soket = io();
+
+  const usernameNode = document.querySelector('#username');
+
+  soket.on('set username', username => {
+    // если передан на сервере объект, то он будет в username
+    usernameNode.textContent = username;
+  });
+
+  // так же можно передвать на сервер:
+  soket.emit('i am here to send something to server', {name: 'Den', age: 27});
 });
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const http = require('http');
+const soketIO = require('socket.io');
+const Moniker = require('moniker');
 
 const app = express();
 const server = http.Server(app);
 const port = process.env.PORT || 3000;
+const io = soketIO(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html');
@@ -14,3 +17,15 @@ app.use(express.static('./client'));
 server.listen(port, () => {
   console.log('listening on *:' + port);
 });
+
+io.on('connection', soket => {
+  // присвоение сокету именя
+  soket.username = Moniker.choose();
+  // emit отправляет сообщению только этому пользователю (c messageType set username)
+  soket.emit(
+    'set username', // type
+    soket.username // payload (в payload можно передавать и объеты) см client 7 строка
+  );
+  // получение с client 30 строки объека и вывод его в консоль на сервере
+  soket.on('i am here to send something to server', iAmUser => console.log(iAmUser.name, iAmUser.age));
+})
